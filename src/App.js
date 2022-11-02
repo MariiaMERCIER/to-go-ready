2817c0b2ab2f6843816038b25036b6fba830f37e
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-11-02 20:00:03 +0100

    switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import react from "./assets/images/react.png";
+import { useState } from "react";
 
 function App() {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+
   return (
     <>
       <header>
@@ -12,17 +17,79 @@ function App() {
           </div>
         </div>
       </header>
+
       <main className="containerMain">
         <div className="form">
           <div className="btnON">
-            <button className="on">ON</button>
-            <button className="on">ON</button>
-            <button className="on">ON</button>
+            <button
+              onClick={() => {
+                switch1 === false && setSwitch1(!switch1);
+              }}
+              style={{
+                backgroundColor: switch1 ? "#5C48D2" : "rgb(229, 229, 229)",
+              }}
+              className="on"
+            >
+              ON
+            </button>
+            <button
+              onClick={() => {
+                switch2 === false && setSwitch2(!switch2);
+              }}
+              style={{
+                backgroundColor: switch2 ? "#5C48D2" : "rgb(229, 229, 229)",
+              }}
+              className="on"
+            >
+              ON
+            </button>
+            <button
+              onClick={() => {
+                switch3 === false && setSwitch3(!switch3);
+              }}
+              style={{
+                backgroundColor: switch3 ? "#5C48D2" : "rgb(229, 229, 229)",
+              }}
+              className="on"
+            >
+              ON
+            </button>
           </div>
+
           <div className="btnOFF">
-            <button className="off">OFF</button>
-            <button className="off">OFF</button>
-            <button className="off">OFF</button>
+            <button
+              onClick={() => {
+                switch1 === true && setSwitch1(!switch1);
+              }}
+              style={{
+                backgroundColor: switch1 ? "#5C48D2" : "rgb(229, 229, 229)",
+              }}
+              className="off"
+            >
+              OFF
+            </button>
+            <button
+              onClick={() => {
+                switch2 === true && setSwitch2(!switch2);
+              }}
+              style={{
+                backgroundColor: switch2 ? "#5C48D2" : "rgb(229, 229, 229)",
+              }}
+              className="off"
+            >
+              OFF
+            </button>
+            <button
+              onClick={() => {
+                switch3 === true && setSwitch3(!switch3);
+              }}
+              style={{
+                backgroundColor: switch3 ? "#5C48D2" : "rgb(229, 229, 229)",
+              }}
+              className="off"
+            >
+              OFF
+            </button>
           </div>
 
           <button className="noWay">NO WAY!</button>
